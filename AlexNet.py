b4d8b6d121a5eac7478f8a327234f5d63079078b
jie <go926315@gmail.com>
2020-04-24 17:11:47 +0800

    update

--- AlexNet.py
+++ AlexNet.py
@@ -1,19 +1,6 @@
-import os
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import torch.optim as optim
-import torchvision
-from torchvision import transforms
-import matplotlib.pyplot as plt
-import numpy as np
-import time
-
-# os.environ["CUDA_VISIBLE_DEVICES"]="1"
-
-device = torch.device("cuda:1")
-print(device)
-
 
 class AlexNet(nn.Module):
     def __init__(self):
@@ -56,96 +43,6 @@ class AlexNet(nn.Module):
         x = self.classifier(x)
         return F.log_softmax(x, dim=1)
         # return x
-    
-def test(model, testloader):
-    model.to(device)
-    correct = 0
-    total = 0
-
-    model.eval()
-    with torch.no_grad():
-        for data in testloader:
-            images, labels = data
-            images, labels = images.to(device), labels.to(device)
-            outputs = model(images)
-            
-            _, predicted = torch.max(outputs.data, 1)
-            total += labels.size(0)
-            correct += (predicted == labels).sum().item()
-            
-
-    print('Accuracy of the network on the 10000 test images: %d %%' % (
-        100 * correct / total))
-
-transform = transforms.Compose(
-[transforms.Resize(256),
-transforms.ToTensor(), 
-transforms.Normalize((0.5,0.5,0.5),(0.5,0.5,0.5)),]
-)
-
-Batch_size = 64
-EPOCH = 20
-
-trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
-                                        download=True, transform=transform)
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=Batch_size,
-                                          shuffle=True, num_workers=0)
-
-testset = torchvision.datasets.CIFAR10(root='./data', train=False,
-                                       download=True, transform=transform)
-testloader = torch.utils.data.DataLoader(testset, batch_size=Batch_size,
-                                         shuffle=False, num_workers=0)
-
-
-# net = torch.load('AlexNet.pkl')
-net = AlexNet()
-print(net)
-net.to(device)
-
-criterion = nn.CrossEntropyLoss()
-optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
-
-train_data = len(trainloader)
-
-end = time.time()
-
-for epochs in range(EPOCH):
-    
-    batch_time_mean = []
-    end = time.time()
-    running_loss = 0.0
-    for i, data in enumerate(trainloader, 0):
-        inputs, labels = data
-        inputs, labels = inputs.to(device), labels.to(device)
-        
-        optimizer.zero_grad()
-        
-        outputs = net(inputs)
-        loss = criterion(outputs, labels)
-        loss.backward()
-        optimizer.step()
-        
-        running_loss += loss.item()
-        
-        batch_time = time.time() - end
-        end = time.time()
-        batch_time_mean.append(batch_time)
-        
-        
-        if i % (int(train_data /10)) == 0 and i > 1:
-            print('[%d, %5d] loss: %.3f' % (epochs+1, i+1, running_loss / (int(train_data /10))))
-            running_loss = 0.0
-
-    
-    print('batch time = %f' % (np.mean(batch_time_mean)))
-    test(net, testloader)
-
-
-
-# torch.save(net, 'AlexNet.pkl')
-
-
-
 
 
 
--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # CNN model
 
 ## AlexNet
+CIFAR10 dataset
+Accuracy of the network on the 10000 test images: 78 %
+
+## VGG 
+CIFAR10 dataset
+Accuracy of the network on the 10000 test images: 89 %
